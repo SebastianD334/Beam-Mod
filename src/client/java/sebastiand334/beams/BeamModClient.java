5fcb298e997a4f282907096983b09995b4335580
Julian Dunskus <julian.dunskus@gmail.com>
2023-06-20 04:42:25 +0200

    render preview in two passes

--- src/client/java/sebastiand334/beams/BeamModClient.java
+++ src/client/java/sebastiand334/beams/BeamModClient.java
@@ -7,18 +7,21 @@ import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.ModelPartBuilder;
-import net.minecraft.client.render.*;
-import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.joml.Vector3f;
 
 public class BeamModClient implements ClientModInitializer {
     @SuppressWarnings("deprecation")
     public static final SpriteIdentifier BEAM_TEXTURE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("beam-mod", "entity/beam_preview"));
 
-    private static final RenderLayer PREVIEW_LAYER = new PreviewLayer();
+    private static final RenderLayer PREVIEW_LAYER_1 = new PreviewLayer("beam_preview_1", true);
+    private static final RenderLayer PREVIEW_LAYER_2 = new PreviewLayer("beam_preview_2", false);
 
     @Override
     public void onInitializeClient() {
@@ -47,24 +50,40 @@ public class BeamModClient implements ClientModInitializer {
         matrices.push();
         matrices.translate(-camera.x, -camera.y, -camera.z);
 
-        matrices.push();
-        matrices.translate(start.x, start.y, start.z);
         var consumers = context.consumers();
         assert consumers != null;
-        mc.getItemRenderer().renderItem(new ItemStack(BeamMod.ROASTED_COCOA_BEAMS), ModelTransformationMode.GROUND, 255, 0, context.matrixStack(), consumers, mc.world, 0);
-        matrices.pop();
 
-        var corner = placement.corner1().toVector3f().mul(16f);
-        var shape = placement.getShape().toVector3f().mul(16f);
+        var corner1 = placement.corner1().toVector3f().mul(16f);
+        var corner2 = placement.corner2().toVector3f().mul(16f);
+        var min = corner1.min(corner2, new Vector3f());
+        var max = corner1.max(corner2); // avoid allocation by reusing corner1
+
+        var epsilon = 1e-3f; // avoid z-fighting
         var beamParts = ModelPartBuilder.create().cuboid(
-            corner.x, corner.y, corner.z,
-            shape.x, shape.y, shape.z
+            min.x - epsilon,
+            min.y - epsilon,
+            min.z - epsilon,
+            max.x - min.x + 2 * epsilon,
+            max.y - min.y + 2 * epsilon,
+            max.z - min.z + 2 * epsilon
         ).build();
-        var buffer = consumers.getBuffer(PREVIEW_LAYER);
-        for (var part : beamParts) {
-            var cuboid = part.createCuboid(1, 1);
-            cuboid.renderCuboid(matrices.peek(), buffer, 255, 0, 1f, 0.5f, 0f, 0.25f);
-        }
+        assert beamParts.size() == 1;
+        var cuboid = beamParts.get(0).createCuboid(1, 1);
+
+        // draw faintly, ignoring depth
+        cuboid.renderCuboid(
+            matrices.peek(),
+            consumers.getBuffer(PREVIEW_LAYER_1),
+            255, 0,
+            1f, 0.4f, 0.2f, 0.1f
+        );
+        // draw more brightly, respecting depth
+        cuboid.renderCuboid(
+            matrices.peek(),
+            consumers.getBuffer(PREVIEW_LAYER_2),
+            255, 0,
+            1f, 0.6f, 0f, 0.2f
+        );
 
         matrices.pop();
     }
@@ -72,7 +91,9 @@ public class BeamModClient implements ClientModInitializer {
     private static class PreviewLayer extends RenderLayer {
         private static final ImmutableList<RenderPhase> phases = ImmutableList.of(COLOR_PROGRAM, ALWAYS_DEPTH_TEST, DISABLE_CULLING, TRANSLUCENT_TRANSPARENCY);
 
-        public PreviewLayer() {
+        private final boolean ignoresDepth;
+
+        public PreviewLayer(String name, boolean ignoresDepth) {
             super(
                 "beam_preview",
                 VertexFormats.POSITION_COLOR,
@@ -83,6 +104,15 @@ public class BeamModClient implements ClientModInitializer {
                 () -> phases.forEach(RenderPhase::startDrawing),
                 () -> phases.forEach(RenderPhase::endDrawing)
             );
+            this.ignoresDepth = ignoresDepth;
+        }
+
+        @Override
+        public void startDrawing() {
+            super.startDrawing();
+            if (this.ignoresDepth) {
+                RenderSystem.disableDepthTest();
+            }
         }
     }
 }
